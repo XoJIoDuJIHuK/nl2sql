da726102d1c3b071ffcc3a14c9e8e5e18ec19b3d
Aleh <XoJIoDuJIHuK@yandex.by>
2025-12-16 10:00:56 +0300

    add async sessionmaker

--- graphql_server/database.py
+++ graphql_server/database.py
@@ -1,11 +1,15 @@
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
+from sqlalchemy.orm import declarative_base
 
 # TODO: Move connection string to environment variables for production
 DATABASE_URL = "postgresql+asyncpg://user:password@localhost/dbname"
 
-engine = create_async_engine(DATABASE_URL, echo=True)
-async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+engine = create_async_engine(DATABASE_URL, echo=True,)
+async_session = async_sessionmaker(
+    engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+)
 
 Base = declarative_base()
 
